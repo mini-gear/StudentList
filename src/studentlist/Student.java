a8bc38b795585f4e3260e8a94d2bc06bbd4824a6
AyaanUlHasan <hasanaya@sheridancollege.ca>
2025-01-15 14:15:37 -0500

    Removed Address property from student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -11,12 +11,11 @@ package studentlist;
 public class Student {
     private String name;
     private String studentID;
-    private String address;
 
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
